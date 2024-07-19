50d3308e07c7ad345fbf2aa1e6898e2dc3299559
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-07-19 14:23:48 -0500

    Added css link

---
+++ js/home.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// function updateTimer() {
+//   if (detonationTimer == 0) {
+//       alert('EXTERMINATE!');
+//       document.body.innerHTML = '';
+//   } else if (detonationTimer > 0) {
+//       document.getElementById('timer').innerHTML = detonationTimer;
+//   }
+
+//   detonationTimer--;
+// }
+// updateTimer()
+let x = document.getElementById('heyoo');
+x.addEventListener("click", function(e){
+  alert("yo mayngs")
+})
